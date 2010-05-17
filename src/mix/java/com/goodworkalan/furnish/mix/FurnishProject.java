e267b971359c17a4db92bbd288205768b0666418
Alan Gutierrez <alan@blogometer.com>
2010-05-16 23:26:11 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/furnish/mix/FurnishProject.java
+++ src/mix/java/com/goodworkalan/furnish/mix/FurnishProject.java
@@ -4,16 +4,24 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class FurnishProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Furnish.
+ *
+ * @author Alan Gutierrez
+ */
+public class FurnishProject implements ProjectModule {
+    /**
+     * Build the project definition for Furnish.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.furnish/furnish/0.1")
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
